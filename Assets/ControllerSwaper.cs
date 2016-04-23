105955c04545e8020c99df8635d96769ea4ab05f
MarianoGNU <marianognu.easyrpg@gmail.com>
2016-04-23 11:37:39 -0300

    First commit

---
+++ Assets/ControllerSwaper.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets;
+
+public class ControllerSwaper : MonoBehaviour {
+
+	FirstPersonController groundController;
+	CharacterController characterController;
+	Rigidbody body;
+	JetPack airController;
+
+	// Use this for initialization
+	void Start () {
+		body = GetComponent<Rigidbody> ();
+		groundController = GetComponent<FirstPersonController> ();
+		characterController = GetComponent<CharacterController> ();
+		airController = GetComponent<JetPack> ();
+	}
+
+	void OnTriggerEnter(Collider col) {
+		if (col.tag == "Terrain") {
+			body.isKinematic = true;
+			body.constraints = 0;
+			airController.enabled = false;
+			groundController.enabled = true;
+			characterController.enabled = true;
+			characterController.Move (new Vector3(0, -1, 0));
+			//characterController.Move (new Vector3(0, 0.01f, 0));
+		}
+	}
+
+	void OnTriggerExit(Collider col) {
+		if (col.tag == "Terrain") {
+			body.isKinematic = false;
+			airController.enabled = true;
+			body.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+			groundController.enabled = false;
+			characterController.enabled = false;
+		}
+	}
+}
---
+++ Assets/JetPack.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class JetPack : MonoBehaviour {
+
+	public Rigidbody jetRidigBody;
+
+	// Audio
+	public AudioMixerSnapshot audioRocketOff;
+	public AudioMixerSnapshot audioRocketOn;
+
+	// Fuel
+	public float fuel = 100f;
+	public float fuelCost = 1f;
+
+	// Jet Up
+	private float rocketPower;
+	public float rocketPowerScale;
+
+	// Jet FWD 
+	private float jetFSpeed;
+
+	// Jet Side
+	private float jetSideSpeed;
+
+	// Jet Turn
+	[HideInInspector]
+	private float jetTurn;
+	public float jetTurnScale;
+
+	// Jet Speed Scale
+	public float jetFSpeedScale;
+
+	// Text
+	public Text fuelText;
+
+	void Start () {
+
+	//	rocketAudio.loop = true;
+
+
+	}
+	
+	// Update is called once per frame
+
+	void OnDisable() {
+		foreach (AudioSource src in GetComponentsInChildren<AudioSource>()) {
+			if (src.gameObject == gameObject)
+				continue;
+			src.volume = 0;
+		}
+	}
+
+	void OnEnable() {
+		jetRidigBody.velocity = GetComponent<CharacterController> ().velocity;
+	}
+
+	void FixedUpdate () {
+
+		rocketPower = Mathf.Abs(Input.GetAxis ("Jet"));
+
+		jetFSpeed = Input.GetAxis ("Vertical");
+
+		jetSideSpeed = Input.GetAxis ("Horizontal"); 
+
+		jetTurn = Input.GetAxis ("RightH");
+
+		fuelText.text = ("Fuel:"+fuel.ToString());
+
+		if (fuel > 0) {
+			
+			// UP
+			jetRidigBody.AddForce (transform.up * rocketPower * rocketPowerScale);
+			fuel -= fuelCost * Mathf.Round (Mathf.Abs (rocketPower));
+			// FWD
+			jetRidigBody.AddRelativeForce (Vector3.forward * jetFSpeed * jetFSpeedScale);
+			fuel -= fuelCost * Mathf.Round (Mathf.Abs (jetFSpeed));
+			// Side
+			jetRidigBody.AddRelativeForce (Vector3.right * jetSideSpeed * jetFSpeedScale);
+			fuel -= fuelCost * Mathf.Round (Mathf.Abs (jetSideSpeed));
+			// Turn
+			jetRidigBody.AddRelativeTorque (Vector3.up * jetTurn * jetTurnScale);
+			fuel -= fuelCost * Mathf.Round (Mathf.Abs (jetTurn));
+
+			float volume = (Mathf.Abs (rocketPower) + Mathf.Abs (jetFSpeed) + Mathf.Abs (jetSideSpeed) + Mathf.Abs (jetTurn))/16;
+
+			foreach (AudioSource src in GetComponentsInChildren<AudioSource>()) {
+				if (src.gameObject == gameObject)
+					continue;
+				src.volume = volume;
+			}
+		}
+	}
+}
